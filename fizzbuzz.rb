57be1d6b87e1ddeed862cf2d551d6f832d856f4f
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-10 09:37:46 +1000

    shorter version

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,3 @@
-# i = 1
-# for i in 1..100
-#   if i % 3 == 0 && i % 5 == 0
-#     puts 'fizzbuzz'
-#   elsif i % 3 == 0
-#     puts 'fizz'
-#   elsif i % 5 == 0
-#     puts 'buzz'
-#   else
-#     puts i
-#   end
-#   i += 1
-# end
-
 (1..100).each do |n|
   print "fizz" if n % 3 == 0
   print "buzz" if n % 5 == 0
